411e310f61f91818e916dbd3c2b1b9f6a707b12e
Robinson Silva <robinson.silva@wps-sa.com.br>
2021-02-08 12:45:49 -0300

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { HighchartsChartComponent } from 'highcharts-angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HighchartsChartComponent
+   ],
+   imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
